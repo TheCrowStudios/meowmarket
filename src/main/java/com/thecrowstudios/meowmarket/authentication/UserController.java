749fd7f8290621e10e2c170e0670d8546bfce48a
TheCrowStudios <madgamersmemes@gmail.com>
2025-02-02 13:50:10 +0000

    some ui changes in account orders page

--- src/main/java/com/thecrowstudios/meowmarket/authentication/UserController.java
+++ src/main/java/com/thecrowstudios/meowmarket/authentication/UserController.java
@@ -2,6 +2,7 @@ package com.thecrowstudios.meowmarket.authentication;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,9 +11,10 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/account")
-    public String account() {
+    public String account(Model model) {
         User user = userService.getUser();
         if (user == null) return "redirect:/api/auth/login";
+        model.addAttribute("user", user);
         return "account";
     }
 }

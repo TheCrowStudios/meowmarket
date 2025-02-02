29747d4dc27801f678c4aa85961acf20d7bb2606
TheCrowStudios <madgamersmemes@gmail.com>
2025-02-02 23:23:06 +0000

    be able to view uploaded images without having to rebuild the application

--- src/main/java/com/thecrowstudios/meowmarket/WebConfig.java
+++ src/main/java/com/thecrowstudios/meowmarket/WebConfig.java
@@ -1,16 +1,31 @@
 package com.thecrowstudios.meowmarket;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${upload.path}")
+    private String uploadPath;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
         .allowedOrigins("*")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload-dir/**")
+        .addResourceLocations("file:" + uploadPath + "/")
+        .setCachePeriod(86400)
+        .resourceChain(true)
+        .addResolver(new PathResourceResolver());
     }
 }

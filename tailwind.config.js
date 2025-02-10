f08b77bb08b1522040847bf1abd297257b455db7
TheCrowStudios <madgamersmemes@gmail.com>
2025-02-10 14:10:13 +0000

    add border arund selected side image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/main/resources/**/*.{html,js,ts}"],
+  safelist: [
+    'border-red-400'
+  ],
   theme: {
     extend: {},
   },

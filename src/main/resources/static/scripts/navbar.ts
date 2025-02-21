344057eb2a543e6934ea6b3e2cba6069cec13909
TheCrowStudios <madgamersmemes@gmail.com>
2025-02-21 23:25:28 +0000

    add dropdown for categories in navbar, make navbar opening animation smoother

--- src/main/resources/static/scripts/navbar.ts
+++ src/main/resources/static/scripts/navbar.ts
@@ -7,12 +7,6 @@ function showUserDropdown() {
 function toggleNavbar() {
     const sideNav = document.getElementById('side-nav');
     const sideNavLinks = document.getElementById('side-nav-links');
-    sideNav?.classList.toggle('w-16');
-    sideNav?.classList.toggle('w-screen');
-    sideNav?.classList.toggle('h-screen');
-    sideNav?.classList.toggle('bg-primary')
-    sideNavLinks?.classList.toggle('w-0');
-    sideNavLinks?.classList.toggle('w-screen');
-    sideNavLinks?.classList.toggle('flex');
-    sideNavLinks?.classList.toggle('hidden');
+    sideNavLinks?.classList.toggle('-translate-x-full');
+    sideNavLinks?.classList.toggle('opacity-0');
 }
